7fb1b697ff639ebb5840e13d7849d83341874d18
Farid <faritfir521@gmail.com>
2023-07-02 07:16:43 +0700

    new feature 003

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const bodyParser = require("body-parser");
 const postReceiver = require("./middleware/clipboard/postData");
 const resultsData = require("./middleware/clipboard/results");
-const qrGenerator = require("./middleware/qr-generator/mainProcces");
+// const qrGenerator = require("./middleware/qr-generator/mainProcces");
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 
 app.post("/post", postReceiver);
 
-app.post("/qr", qrGenerator);
+// app.post("/qr", qrGenerator);
 
 app.post("/results", resultsData);
 
